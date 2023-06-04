067795ec016f2edbd0f574126fffa75c9d0b6447
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-04 17:23:30 +0330

    did 4th part

--- main.go
+++ main.go
@@ -25,10 +25,16 @@ func handleName1(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello " + name))
 }
 
+func handleName2(w http.ResponseWriter, r *http.Request) {
+	name := r.Header.Get("name")
+	w.Write(append([]byte("hello "), name...))
+}
+
 func main() {
 	http.HandleFunc("/hello", handleHello)
 	http.HandleFunc("/bye", handleBye)
 	http.HandleFunc("/name1", handleName1)
+	http.HandleFunc("/name2", handleName2)
 	err := http.ListenAndServe(HOST+":"+PORT, nil)
 	if err != nil {
 		panic(err)
