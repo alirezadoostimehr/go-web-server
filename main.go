779b6414311aa53e4648f78b1e2296bc7e6ef583
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-04 17:07:47 +0330

    did 2nd part

--- main.go
+++ main.go
@@ -14,8 +14,14 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello"))
 }
 
+func handleBye(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadGateway)
+	w.Write([]byte("bye"))
+}
+
 func main() {
 	http.HandleFunc("/hello", handleHello)
+	http.HandleFunc("/bye", handleBye)
 	err := http.ListenAndServe(HOST+":"+PORT, nil)
 	if err != nil {
 		panic(err)
